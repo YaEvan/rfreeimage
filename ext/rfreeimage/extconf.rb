dbf2c000db125f0c5819099b86181d119b201828
yangyi <yangyi2048@outlook.com>
2022-01-11 14:33:45 +0800

    Support gcc7.5 for linux

--- ext/rfreeimage/extconf.rb
+++ ext/rfreeimage/extconf.rb
@@ -3,29 +3,29 @@ require 'mkmf'
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
 $CFLAGS << " #{ENV["CFLAGS"]}"
-$CFLAGS << " -std=gnu89"
+$CFLAGS << " -std=gnu++98"
 $CFLAGS << " -g"
 $CFLAGS << " -O3" unless $CFLAGS[/-O\d/]
 $CFLAGS << " -Wall -Wno-comment"
 $LDFLAGS << "-Wl,--exclude-libs=ALL"
 
 def sys(cmd)
-	puts " -- #{cmd}"
-	unless ret = xsystem(cmd)
-		raise "ERROR: '#{cmd}' failed"
-	end
-	ret
+  puts " -- #{cmd}"
+  unless ret = xsystem(cmd)
+    raise "ERROR: '#{cmd}' failed"
+  end
+  ret
 end
 
 if !(MAKE = find_executable('gmake') || find_executable('make'))
-	abort "ERROR: GNU make is required to build Rugged."
+  abort "ERROR: GNU make is required to build Rugged."
 end
 
 CWD = File.expand_path(File.dirname(__FILE__))
 FREEIMAGE_DIR = File.join(CWD, '..', '..', 'vendor', 'FreeImage')
 
 Dir.chdir(FREEIMAGE_DIR) do
-	sys(MAKE)
+  sys(MAKE)
 end
 
 $DEFLIBPATH.unshift("#{FREEIMAGE_DIR}/Dist")
